f3cdc45eb7e5faaa0687ade9f1a1fa21b8a2b06f
asheshvidyut <134911583+asheshvidyut@users.noreply.github.com>
2025-06-07 15:36:59 +0000

    Automated change: Fix sanity tests

--- src/python/grpcio/grpc/_observability.py
+++ src/python/grpcio/grpc/_observability.py
@@ -18,7 +18,7 @@ import abc
 import contextlib
 import logging
 import threading
-from typing import Any, Generator, Generic, List, Optional, TypeVar, Tuple
+from typing import Any, Generator, Generic, List, Optional, Tuple, TypeVar
 
 from grpc._cython import cygrpc as _cygrpc
 from grpc._typing import ChannelArgumentType
@@ -281,8 +281,9 @@ def maybe_record_rpc_latency(state: "_channel._RPCState") -> None:
             )
 
 
-def create_server_call_tracer_factory_option(xds: bool) -> tuple[tuple[
-  str, ServerCallTracerFactory]] | tuple[()]:
+def create_server_call_tracer_factory_option(
+    xds: bool,
+) -> tuple[tuple[str, ServerCallTracerFactory]] | tuple[()]:
     with get_plugin() as plugin:
         if plugin and plugin.stats_enabled:
             server_call_tracer_factory_address = (
