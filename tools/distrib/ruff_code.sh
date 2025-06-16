5d28f6d1d7f3ccd0f8af15ad0d21bdac5eeeebf9
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-16 10:13:38 +0000

    add 2 more files

--- tools/distrib/ruff_code.sh
+++ tools/distrib/ruff_code.sh
@@ -21,9 +21,8 @@ cd "$(dirname "$0")/../.."
 DIRS=(
     'src/python/grpcio/grpc/_auth.py'
     'src/python/grpcio/grpc/_channel.py'
-    'src/python/gprcio/grpc/__init__.py',
-    'src/python/gprcio/grpc/_common.py',
-    'src/python/gprcio/grpc/_compression.py',
+    'src/python/grpcio/grpc/_common.py'
+    'src/python/grpcio/grpc/_compression.py'
 )
 
 VIRTUALENV=venv_ruff
