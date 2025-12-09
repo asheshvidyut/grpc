59d2891fc44313631a21e210b7897ca6cd808723
asheshvidyut <134911583+asheshvidyut@users.noreply.github.com>
2025-12-09 12:14:46 +0000

    Automated change: Fix sanity tests

--- src/python/grpcio/grpc/__init__.py
+++ src/python/grpcio/grpc/__init__.py
@@ -20,19 +20,17 @@ import enum
 import logging
 import sys
 import types
+from typing import TYPE_CHECKING, Any, Callable, Iterable, Mapping, Optional
 
-from typing import Optional, Any, Callable, Mapping, Iterable, TYPE_CHECKING
 from grpc import _compression
 from grpc._cython import cygrpc as _cygrpc
 from grpc._runtime_protos import protos
 from grpc._runtime_protos import protos_and_services
 from grpc._runtime_protos import services
-from grpc._typing import (
-    MetadataType,
-    NullaryCallbackType,
-    SerializingFunction,
-    DeserializingFunction,
-)
+from grpc._typing import DeserializingFunction
+from grpc._typing import MetadataType
+from grpc._typing import NullaryCallbackType
+from grpc._typing import SerializingFunction
 
 if TYPE_CHECKING:
     import types
@@ -1417,7 +1415,9 @@ class GenericRpcHandler(abc.ABC):
     """An implementation of arbitrarily many RPC methods."""
 
     @abc.abstractmethod
-    def service(self, handler_call_details: HandlerCallDetails) -> Optional[RpcMethodHandler]:
+    def service(
+        self, handler_call_details: HandlerCallDetails
+    ) -> Optional[RpcMethodHandler]:
         """Returns the handler for servicing the RPC.
 
         Args:
