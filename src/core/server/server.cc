db81b3b84d7de78ba115c93d656e429108955eb5
asheshvidyut <134911583+asheshvidyut@users.noreply.github.com>
2026-04-28 16:30:09 +0000

    Automated change: Fix sanity tests

--- src/core/server/server.cc
+++ src/core/server/server.cc
@@ -579,7 +579,8 @@ class Server::RealRequestMatcher : public RequestMatcherInterface {
  public:
   explicit RealRequestMatcher(Server* server)
       : server_(server), requests_per_cq_(server->cqs_.size()) {
-    LOG(ERROR) << "RealRequestMatcher constructor: server->cqs_.size()=" << server->cqs_.size()
+    LOG(ERROR) << "RealRequestMatcher constructor: server->cqs_.size()="
+               << server->cqs_.size()
                << ", requests_per_cq_.size()=" << requests_per_cq_.size();
   }
 
@@ -683,7 +684,8 @@ class Server::RealRequestMatcher : public RequestMatcherInterface {
 
   void MatchOrQueue(size_t start_request_queue_index,
                     CallData* calld) override {
-    LOG(ERROR) << "MatchOrQueue: start_request_queue_index=" << start_request_queue_index
+    LOG(ERROR) << "MatchOrQueue: start_request_queue_index="
+               << start_request_queue_index
                << ", requests_per_cq_.size()=" << requests_per_cq_.size();
     if (requests_per_cq_.empty()) {
       calld->SetState(CallData::CallState::PENDING);
@@ -730,7 +732,8 @@ class Server::RealRequestMatcher : public RequestMatcherInterface {
 
   ArenaPromise<absl::StatusOr<MatchResult>> MatchRequest(
       size_t start_request_queue_index) override {
-    LOG(ERROR) << "MatchRequest: start_request_queue_index=" << start_request_queue_index
+    LOG(ERROR) << "MatchRequest: start_request_queue_index="
+               << start_request_queue_index
                << ", requests_per_cq_.size()=" << requests_per_cq_.size();
     if (requests_per_cq_.empty()) {
       return Immediate(absl::InternalError("Server closed"));
