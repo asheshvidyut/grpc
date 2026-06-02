aa2f62495756f14ee564a6ebd12575b881ecef2a
Ashesh Vidyut <asheshvidyut@google.com>
2026-06-02 20:59:00 +0530

    fix for macosx

--- src/python/grpcio_tests/tests_aio/unit/auth_context_test.py
+++ src/python/grpcio_tests/tests_aio/unit/auth_context_test.py
@@ -106,20 +106,29 @@ class TestAuthContext(AioTestBase):
         server = aio.server()
         server.add_generic_rpc_handlers((handler,))
         server_cred = grpc.ssl_server_credentials(_SERVER_CERTS)
-        port = server.add_secure_port("[::]:0", server_cred)
+
+        bind_addr = "127.0.0.1:0" if __import__('sys').platform == 'darwin' else "[::]:0"
+        port = server.add_secure_port(bind_addr, server_cred)
+
         await server.start()
 
         channel_creds = grpc.ssl_channel_credentials(
             root_certificates=_TEST_ROOT_CERTIFICATES
         )
+
+        target_addr = "127.0.0.1:{}" if __import__('sys').platform == 'darwin' else "localhost:{}"
         channel = aio.secure_channel(
-            ("127.0.0.1:{}" if __import__('sys').platform == 'darwin' else "localhost:{}").format(port),
+            target_addr.format(port),
             channel_creds,
             options=_PROPERTY_OPTIONS,
         )
-        response = await channel.unary_unary(_UNARY_UNARY)(_REQUEST)
+
+        response = await channel.unary_unary(_UNARY_UNARY)(
+            _REQUEST, wait_for_ready=True
+        )
         await channel.close()
-        await server.stop(None)
+
+        await server.stop(0)
 
         auth_data = pickle.loads(response)
         self.assertIsNone(auth_data[_ID])
