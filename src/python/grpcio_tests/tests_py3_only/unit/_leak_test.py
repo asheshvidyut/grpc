8da0b487abd8cb22faef03c8a2d12380fcca5e44
Ashesh Vidyut <asheshvidyut@google.com>
2026-06-03 06:38:42 +0530

    Increase Darwin leak_test fail threshold to 15 MiB

--- src/python/grpcio_tests/tests_py3_only/unit/_leak_test.py
+++ src/python/grpcio_tests/tests_py3_only/unit/_leak_test.py
@@ -31,7 +31,7 @@ _TEST_METHOD = "/test/Test"
 _REQUEST = b"\x23\x33"
 if sys.platform == "darwin":
     _LARGE_NUM_OF_ITERATIONS = 500
-    _FAIL_THRESHOLD = 5 * 1024 * 1024  # 5 MiB
+    _FAIL_THRESHOLD = 15 * 1024 * 1024  # 15 MiB
 else:
     _LARGE_NUM_OF_ITERATIONS = 5000
     _FAIL_THRESHOLD = 25 * 1024 * 1024  # 25 MiB
